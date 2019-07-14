4c68ac05f98e07e87e60e9b49cb4ec07db9f72a4
Daniel <bellissimo38@gmail.com>
2019-07-13 20:17:01 -0400

    Deployment script

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+
+javac HostServer.java
+javac Client_Instance.java
+scp HostServer.class danielbellissimo.ca:
+scp Client_Instance.class danielbellissimo.ca:
+echo "Deployment to danielbellissimo.ca completed"
